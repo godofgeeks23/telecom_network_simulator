48c4d1f20ab93ff2cb04365a236f97805427a590
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-09-21 23:32:42 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,12 +1,28 @@
-import datetime
-start = "00:00:00"
-end = "23:59:00"
-delta = datetime.timedelta(minutes=15)
-start = datetime.datetime.strptime( start, '%H:%M:%S' )
-end = datetime.datetime.strptime( end, '%H:%M:%S' )
-t = start
-timestamps = []
-while t <= end :
-     timestamps.append(datetime.datetime.strftime( t, '%H:%M:%S'))
-     t += delta
-print(timestamps)
+# import datetime
+# start = "00:00:00"
+# end = "23:59:00"
+# delta = datetime.timedelta(minutes=15)
+# start = datetime.datetime.strptime( start, '%H:%M:%S' )
+# end = datetime.datetime.strptime( end, '%H:%M:%S' )
+# t = start
+# timestamps = []
+# while t <= end :
+#      timestamps.append(datetime.datetime.strftime( t, '%H:%M:%S'))
+#      t += delta
+# print(timestamps)
+
+
+
+from datetime import datetime, timedelta
+
+def datetime_range(start, end, delta):
+    current = start
+    while current < end:
+        yield current
+        current += delta
+
+dts = [dt.strftime('%Y-%m-%d %H:%M:%S') for dt in 
+       datetime_range(datetime(2022, 9, 21, 0), datetime(2022, 9, 21+2, 00), 
+       timedelta(minutes=15))]
+
+print(dts)
